d36218208e11bf7d2dacd6fdca2fa1ad8407ac03
Jingw Yang <15210700048@fudan.edu.cn>
2017-12-24 16:02:07 +0800

    first commit

---
+++ R/AnceTran.R
@@ -0,0 +1,16 @@
+#'
+#' @title AnceTran: the package for phylogenetic analysis of TF-binding data
+#'
+#' @description \code{AnceTran} is an R package that performs TF-binding phylogenetic analysis
+#' from ChIP-seq data, including optimized input formatting, normalization
+#' and pair-wise distance evaluation, ancestral TF-binding state inference.
+#' @name AnceTran
+#'
+#' @docType package
+#'
+#' @import ape
+#' @import phytools
+#'
+#' @author Jingw Yang (jingwyang.fudan@gmail.com).
+
+NULL
