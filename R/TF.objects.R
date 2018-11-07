c47d37be9ba9b4e2b5940316197173f6a3f44fab
jingwenyang <jingw.yang.fudan@gmail.com>
2018-11-07 18:05:53 +0800

    Add DATA

---
+++ R/TF.objects.R
@@ -0,0 +1,21 @@
+#' @title taxaTF object created from HNF4A binding data for 4 mice species
+#'
+#' @name TF.objects
+#'
+#' @description large \code{taxaTF} object created from
+#' HNF4A-binding data of four mice species
+#'
+#' @docType data
+#'
+#' @format a \code{taxaTF} object, contains 4 \code{taxonExp}:
+#' with raw binding score values on 22698 genes.
+#'
+#' @references
+#' Wong,ES. et al. (2015) Decoupling of evolutionary changes
+#' in transcription factor binding and gene expression in mammals. Genome Res 25: 167-178.
+#'
+#' @examples
+#' data(TF.objects)
+#' TF.objects
+#' TF.objects[[1]]
+NULL
